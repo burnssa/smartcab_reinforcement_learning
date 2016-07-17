a5c1e43af3a6243e52677f557cb3931cd37b5b4b
burnssa <scott@guidefinancial.com>
2016-07-16 23:25:08 -0700

    add q-learning table

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -2,6 +2,8 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+import pdb
+import math
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -10,6 +12,10 @@ class LearningAgent(Agent):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        self.q_table = {}
+        self.states = []
+        # self.previous_steps_forward = 0
+        # self.previous_directional_turns = 0
         # TODO: Initialize any additional variables here
 
     def reset(self, destination=None):
@@ -22,17 +28,56 @@ class LearningAgent(Agent):
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
-        # TODO: Update state
-        
-        # TODO: Select action according to your policy
-        action = None
+        # TODO: Update state + add other variables to inputs
+        state = inputs
+        #set id for state do make more tractable
+        if state not in self.states:
+            self.states.append(inputs)
 
+        state_index = self.states.index(state)
+        if state_index not in self.q_table.keys():
+            self.q_table[state_index] = {}
+            self.q_table[state_index]['action_reward'] = {}
+
+        # TODO: Select action according to your policy
+        action = self.choose_action(self.q_table, self.states, state)
+
+        action = None if action == 'None' else action
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
+    
+        self.q_table[state_index]['action_reward'][action] = reward
+        print self.q_table
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+
+
+    # def find_or_create_state(self, states, inputs):
+    #     if inputs in states:
+    #         state = states.index(inputs)
+    #     else:
+    #         states.append(inputs):
+    #     return states
+
+    def choose_action(self, q_table, states, state):
+        #TODO: add proper random factor to prevent always responding to state in same way
+        relevant_q_table = q_table[states.index(state)]
+        if not relevant_q_table['action_reward']:
+            action = self.random_action()
+        else:
+            rewards = relevant_q_table['action_reward'].values()
+            best_reward = max(rewards)
+            action = relevant_q_table['action_reward'].keys()[relevant_q_table['action_reward'].values().index(best_reward)]  #choosing first of best rewards - in case two actions return identical rewards
+        return action
+
+    def random_action(self):
+        actions = ['None', 'forward', 'left', 'right']
+        index = int(math.floor(random.random()*4))
+        action = actions[index]
+        return action
+
 
 
 def run():
@@ -45,10 +90,10 @@ def run():
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.1, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
-    sim.run(n_trials=100)  # run for a specified number of trials
+    sim.run(n_trials=10)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
 
